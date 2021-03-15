7632e086c406d4a8bcc77914ba02edc2457b8bf7
Oleg <Kornyakovo@gmail.com>
2021-03-16 00:36:04 +0400

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+exports.min = function min(array) {
+    var min = array[0];
+    if (array.length > 0) {
+        for (var i = 0; i != array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    var max = array[0];
+    if (array.length != true) {
+        for (var i = 0; i != array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+    return 0;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    var avg = 0;
+
+    for (var i = 0; i != array.length; i++) {
+        avg += array[i];
+    }
+    return (avg = avg / array.length);
+};
